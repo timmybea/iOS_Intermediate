733336f8f75283ecef1f0b8812f1de8777e9aa55
Tim Beals <tim.beals@gmail.com>
2017-05-31 12:42:39 -0700

    create initial project

---
+++ 2_Animations/BrainTeaser/BrainTeaser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrainTeaser
+//
+//  Created by Tim Beals on 2017-05-31.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
